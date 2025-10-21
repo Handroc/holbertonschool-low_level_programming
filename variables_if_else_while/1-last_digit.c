2fb14bb38a0106b31e48369f994fe4bda3a405f7
Handroc <andricassani42069@gmail.com>
2025-10-21 10:36:00 +0200

    feat: determines whether the last digit of a random number is null or greater than 5

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - prints the last digit of a randomly generated number
  *
@@ -24,6 +23,6 @@ int main(void)
 	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-	}	
+	}
 	return (0);
 }
