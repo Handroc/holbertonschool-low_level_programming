96357f260c77d6d82bf0e0a8df5f84098ae37059
Handroc <andricassani42069@gmail.com>
2025-10-22 14:49:45 +0200

    fix: added the print_alphabet_x10 function to the header file

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,10 +2,22 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c);
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: 
+ */
 void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ *
+ * Return: 
+ */
+void print_alphabet_x10(void);
