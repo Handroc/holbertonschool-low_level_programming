bbb8bd755a3b6e184218952d3f5e3625e8a689cd
Handroc <andricassani42069@gmail.com>
2025-10-22 15:10:35 +0200

    fix: added the _isalpha function to the header file

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
- * &c: The character to print
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately
@@ -26,3 +29,12 @@ void print_alphabet_x10(void);
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c);
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character to be checked
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c);
+
+#endif
