efa4bc6a159623cb047b9b271f2f1fe492fe84ce
Handroc <andricassani42069@gmail.com>
2025-10-22 15:03:12 +0200

    fix: added the _islower function to header file

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
+ * &c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately
@@ -18,3 +19,10 @@ void print_alphabet(void);
  * Return: On success 1.
  */
 void print_alphabet_x10(void);
+/**
+ * _islower - checks for lowercase character
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c);
