a6ad79519544ab07a2b42b5b76e242fbc53d3ea6
Handroc <andricassani42069@gmail.com>
2025-10-23 14:51:04 +0200

    feat: return the absolute value of a number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - return the absolute value of a number
+ * @n: number to be checked
+ *
+ * Return: n if the number is positive, -n if it's negative
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
