fd70a2497599e5cbe5b0c078e88d8113bdf80d1b
Handroc <andricassani42069@gmail.com>
2025-10-24 15:03:25 +0200

    fix: used standard library instead of main.h

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,73 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
+ * @n: number to be checked
  */
 void print_to_98(int n)
 {
-	int inv;
-	while (n > 98)
-	{
-		if (n > 99)
-		{
-			_putchar('0' + n / 100);
-			_putchar('0' + (n / 10) % 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n == 99)
-		{
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		if (n > 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		n = n - 1;
-	}
 	while (n < 98)
 	{
-		inv = -n;
-		if (n < -99)
-		{
-			_putchar('-');
-			_putchar('0' + inv / 100);
-			_putchar('0' + (inv / 10) % 10);
-			_putchar('0' + inv % 10);
-		}
-		else if (n < -9)
-		{
-			_putchar('-');
-			_putchar('0' + inv / 10);
-			_putchar('0' + inv % 10);
-		}
-		else if (n < 0)
-		{
-			_putchar('-');
-			_putchar('0' + inv);
-		}
-		else if (n < 10)
-		{
-			_putchar('0' + n);
-		}
-		else if (n < 98)
-		{
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		if (n < 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		n = n + 1;
+		printf("%d, ", n);
+		n++;
 	}
-	if (n == 98)
+	while (n > 98)
 	{
-		_putchar('0' + n / 10);
-		_putchar('0' + n % 10);
+		printf("%d, ", n);
+		n--;
 	}
-
-	_putchar('\n');
+	putchar('9');
+	putchar('8');
+	putchar('\n');
 }
