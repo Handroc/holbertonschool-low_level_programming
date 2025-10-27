9bb9d0530c591cddf73342ed92a36969092bce12
Handroc <andricassani42069@gmail.com>
2025-10-27 11:46:12 +0100

    feat: Fizz-Buzz test

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - FizzBuzz test
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n < 101; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
