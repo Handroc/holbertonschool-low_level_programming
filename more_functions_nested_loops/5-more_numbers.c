3ae4574dec4f108f20ce71b08d6bbabaa85c2579
Handroc <andricassani42069@gmail.com>
2025-10-27 10:32:33 +0100

    feat: prints the numbers from 0 to 14 ten times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers from 0 to 14 ten times
+ */
+void more_numbers(void)
+{
+	int j, i;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i >= 10)
+				_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+		}
+		_putchar('\n');
+	}
+}
