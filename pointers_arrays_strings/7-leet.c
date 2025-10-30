6241c523696825a65c7845e8115d1cdda3e17bc8
Handroc <andricassani42069@gmail.com>
2025-10-30 10:01:50 +0100

    feat: encodes a string in 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+/**
+ * leet - encodes a string into 1337.
+ * @a: string
+ *
+ * Return: the encoded string
+ */
+
+char *leet(char *a)
+{
+	char *s1 = "aAeEoOtTlL", *s2 = "43071";
+	int i, k;
+	for (i = 0; i <= 9; i++)
+	{
+		k = 0;
+		while (a[k] != '\0')
+		{
+			if (s1[i] == a[k])
+				a[k] = s2[i / 2];
+			k++;
+		}
+	}
+	return (a);
+}
