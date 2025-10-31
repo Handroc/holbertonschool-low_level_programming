e53e47239a705b10a7b081ba5ada21177f9d00b8
Handroc <andricassani42069@gmail.com>
2025-10-31 10:57:12 +0100

    feat: prints an integer

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: integer
+ */
+void print_number(int n)
+{
+	int tmp, tmp1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (n >= 10)
+	{
+		tmp = n;
+		tmp1 = 1;
+		while (tmp >= 10)
+			tmp = tmp / 10;
+		_putchar('0' + tmp);
+		while (n > tmp1 * 10)
+			tmp1 = tmp1 * 10;
+		if ((n / (tmp1 / 10) % 10) == 0)
+			_putchar('0' + 0);
+		n = n % tmp1;
+	}
+	_putchar('0' + n);
+}
