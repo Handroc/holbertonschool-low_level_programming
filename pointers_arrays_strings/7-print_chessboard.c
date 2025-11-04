589e7e82e8b49eed748df950548ed80210a853ac
Handroc <andricassani42069@gmail.com>
2025-11-04 10:00:50 +0100

    feat: prints a chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,6 +1,16 @@
-
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: 2D array representing the chessboard
+ */
 void print_chessboard(char (*a)[8])
 {
-	while (*a)
+	int i, j;
 
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
 }
