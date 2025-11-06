d580a815e7710b9ec50d5e22ff7901462a3e533d
Handroc <andricassani42069@gmail.com>
2025-11-06 19:44:59 +0100

    feat: finds a prime integer

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+/**
+ * is_prime_helper - helper function to check for primality
+ * @n: number to be checked
+ * @i: current divisor
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_helper(int n, int i)
+{
+	if (n == i)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (is_prime_helper(n, i + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: number to be checked
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_prime_helper(n, 2));
+}
