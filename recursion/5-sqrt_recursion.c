b444867d8c3b40eae9721a9a88c202bbe7d2c6a3
Handroc <andricassani42069@gmail.com>
2025-11-06 19:44:20 +0100

    feat: returns the natural square root of a number

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,4 +1,19 @@
 /**
+ * _sqrt_helper - helper function to find the square root recursively
+ * @n: number to calculate the square root of
+ * @i: current guess for the square root
+ *
+ * Return: natural square root of n or -1 if not a perfect square
+ */
+int  _sqrt_helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (_sqrt_helper(n, i + 1));
+}
+/**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: number to calculate the square root of
  *
@@ -6,17 +21,9 @@
  */
 int _sqrt_recursion(int n)
 {
-	int i = 1;
-
 	if (n < 0)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	while (i * i <= n)
-	{
-		if (i * i == n)
-			return (i);
-		i++;
-	}
-	return (-1);
+	return (_sqrt_helper(n, 0));
 }
