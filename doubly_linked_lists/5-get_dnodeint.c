60525824a3c443174490f9424680bd7842d7c9ad
Handroc <andricassani42069@gmail.com>
2025-12-04 11:29:03 +0100

    fix

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -5,18 +5,27 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *node;
 	unsigned int i = 0;
 
-	while (i < index)
+	if (index == 0)
+		node = head;
+	if (head)
 	{
-		if (head->next)
+		while (i < index)
 		{
-			node = head->next;
-			head = node;
+			if (head->next)
+			{
+				node = head->next;
+				head = node;
+			}
+			else
+			{
+				return (NULL);
+			}
+			i++;
 		}
-		else
-		{
-			return (NULL);
-		}
-		i++;
+	}
+	else
+	{
+		return (NULL);
 	}
 
 	return (node);
