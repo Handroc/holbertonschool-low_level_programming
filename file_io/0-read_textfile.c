822038e94c70897566d1416cff6516de1ea05f89
Handroc <andricassani42069@gmail.com>
2025-12-05 15:31:52 +0100

    fix: case error

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
  *         0 if the file cannot be opened or read, or if filename is NULL
  */
 ssize_t read_textfile(const char *filename, size_t letters)
-{	
+{
 	int fd;
 	char *tmp;
 	ssize_t bytes_r;
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
@@ -36,13 +36,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	bytes_w = write(1, tmp, bytes_r);
+	bytes_w = write(STDOUT_FILENO, tmp, bytes_r);
 
 	free(tmp);
 	close(fd);
 
 	if (bytes_w == -1 || bytes_r != bytes_w)
 		return (0);
-	
+
 	return (bytes_w);
 }
