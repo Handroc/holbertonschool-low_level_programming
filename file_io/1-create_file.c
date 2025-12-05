0a6efe3c45d1a8af68cbee41ff65e1e8f647f357
Handroc <andricassani42069@gmail.com>
2025-12-05 23:09:01 +0100

    fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,8 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len;
+	int fd, len = 0;
+	ssize_t w;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 			;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
